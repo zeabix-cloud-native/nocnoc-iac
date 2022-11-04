1bad9a33cbb3017e9c9e4c4bf7d4623eba9ea9e6
amornc <mornster@gmail.com>
2022-11-04 21:09:32 +0700

    test sprit jobs

--- outputs.tf
+++ outputs.tf
@@ -13,12 +13,12 @@
 #  value       = module.eks.cluster_security_group_id
 #}
 #
-#output "region" {
-#  description = "AWS region"
-#  value       = var.region
-#}
-#
-#output "cluster_name" {
-#  description = "Kubernetes Cluster Name"
-#  value       = module.eks.cluster_name
-#}
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = var.cluster_name
+}
