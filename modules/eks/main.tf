bc9004c859045b48123d751c3d69fec37887b1e5
amornc <mornster@gmail.com>
2022-11-05 21:02:23 +0700

    add blog-service

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -18,7 +18,6 @@ data "aws_eks_cluster_auth" "this" {
 
 module "eks_blueprints" {
   source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.14.0"
-#  source = "github.com/aws-ia/terraform-aws-eks-blueprints"
 
 
   cluster_name    = var.cluster_name
@@ -58,17 +57,17 @@ module "eks_blueprints_kubernetes_addons" {
   eks_cluster_version  = module.eks_blueprints.eks_cluster_version
 
   # EKS Managed Add-ons
-##  enable_amazon_eks_vpc_cni            = true
-##  enable_amazon_eks_coredns            = true
-##  enable_amazon_eks_kube_proxy         = true
-##  enable_amazon_eks_aws_ebs_csi_driver = true
+  enable_amazon_eks_vpc_cni            = true
+  enable_amazon_eks_coredns            = true
+  enable_amazon_eks_kube_proxy         = true
+  enable_amazon_eks_aws_ebs_csi_driver = true
 
   # Add-ons
-##  enable_aws_load_balancer_controller = true
-##  enable_metrics_server               = true
-##  enable_aws_cloudwatch_metrics       = true
-##  enable_kubecost                     = true
-##  enable_kube_prometheus_stack        = true
+  enable_aws_load_balancer_controller = true
+  enable_metrics_server               = true
+  enable_aws_cloudwatch_metrics       = true
+  enable_kubecost                     = true
+  enable_kube_prometheus_stack        = true
 
   enable_cluster_autoscaler = true
   cluster_autoscaler_helm_config = {
@@ -91,17 +90,17 @@ module "eks_blueprints_kubernetes_addons" {
     ]
   }
   enable_cert_manager_csi_driver = true
-##  enable_argocd = true
+  enable_argocd = true
   # This example shows how to set default ArgoCD Admin Password using SecretsManager with Helm Chart set_sensitive values.
-##  argocd_helm_config = {
-##    set_sensitive = [
-##      {
-##        name  = "configs.secret.argocdServerAdminPassword"
-##        value = bcrypt(var.argocd_password)
-##      }
-##    ]
-##  }
-##
-##  argocd_manage_add_ons = true # Indicates that ArgoCD is responsible for managing/deploying add-ons
+  argocd_helm_config = {
+    set_sensitive = [
+      {
+        name  = "configs.secret.argocdServerAdminPassword"
+        value = bcrypt(var.argocd_password)
+      }
+    ]
+  }
+
+  argocd_manage_add_ons = true # Indicates that ArgoCD is responsible for managing/deploying add-ons
 
 }
