ee0f6bfdd045f52b0c21d82d4d51a7161b344595
amornc <mornster@gmail.com>
2022-11-07 09:41:53 +0700

    find ELB

---
+++ k6/http_get.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+  return {
+    "new2.html": htmlReport(data), // show report in html based format.
+    'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout format...
+  };
+}
+
+export const options = {
+  vus: 100,
+  duration: '10m',
+};
+export default function () {
+  http.get('http://127.0.0.1:8080/blogs/v1/blogs');
+  sleep(1);
+}
