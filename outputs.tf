4261acbe90b4a17e50ebe2803f3611e316a73d91
amornc <mornster@gmail.com>
2022-11-07 13:24:34 +0700

    get elb dnsname

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,8 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = "${var.project_name}-cluster"
 }
+
+output "vpc_id" {
+  description = "Kubernetes VPC ID"
+  value       = module.network.vpc_id
+}
