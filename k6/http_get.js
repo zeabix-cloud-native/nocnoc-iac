d5bba949005258cdbe89216a7b2da636d95eedd4
amornc <mornster@gmail.com>
2022-11-07 20:13:53 +0700

    test k6-1

--- k6/http_get.js
+++ k6/http_get.js
@@ -1,12 +1,23 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
 export const options = {
   vus: 100,
   duration: '10m',
 };
 export default function () {
-//  http.get('http://127.0.0.1:8080/blogs/v1/blogs');
-  http.get('https://127.0.0.1/');
+//  http.get('https://127.0.0.1:8080/blogs/v1/blogs');
+//  http.get('https://www.gec.co.th');
+  http.get('https://127.0.0.1');
   sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "result.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  };
 }
