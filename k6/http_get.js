1d3d8e71d576bee74cc5bab663a18e584daf47b3
amornc <mornster@gmail.com>
2022-11-07 18:00:56 +0700

    test k6

--- k6/http_get.js
+++ k6/http_get.js
@@ -14,6 +14,7 @@ export const options = {
   duration: '10m',
 };
 export default function () {
-  http.get('http://127.0.0.1:8080/blogs/v1/blogs');
+//  http.get('http://127.0.0.1:8080/blogs/v1/blogs');
+  http.get('http://127.0.0.1/');
   sleep(1);
 }
